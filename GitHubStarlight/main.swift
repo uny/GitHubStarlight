a3775713f7ebaa1a3ecf66c425d522a8ae7fa892
Yuki Nagai <ynagai@r.recruit.co.jp>
2016-03-24 00:46:16 +0900

    main

---
+++ GitHubStarlight/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  GitHubStarlight
+//
+//  Created by Yuki Nagai on 3/24/16.
+//  Copyright © 2016 Yuki Nagai. All rights reserved.
+//
+
+import Foundation
+
+func main(arguments: [String]) {
+    print(arguments)
+}
+main(Process.arguments)
